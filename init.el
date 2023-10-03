db68821d6f05cd11d2f58ba04d4824456d2b9912
Metin Çetin <metinc309@gmail.com>
2023-10-03 04:50:04 +0300

    improvements

--- init.el
+++ init.el
@@ -12,20 +12,93 @@
 
 
 
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(git-gutter git-gutter+ diff-hl evil-collection haxe-mode battle-haxe expand-region iedit which-key flycheck csharp-mode lsp-ivy lsp-ui avy counsel ace-jump-mode beacon projectile general lsp-mode company vertico evil use-package)))
+   '(vterm-toggle vterm reverse-theme git-gutter git-gutter+ diff-hl evil-collection haxe-mode battle-haxe expand-region iedit which-key flycheck csharp-mode lsp-ivy lsp-ui avy counsel ace-jump-mode beacon projectile general lsp-mode company vertico evil use-package)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(ivy-minibuffer-match-face-2 ((t (:background "gray20")))))
 
+
+
+;; loading themes. first things first
+(load "~/.emacs.d/theming.el")
+
+
+(use-package cc-mode)
+(use-package evil
+  :init
+  (setq evil-want-keybinding nil)
+  :config
+  (evil-mode 1)
+  (define-key evil-motion-state-map " " nil)
+)
+
+(use-package beacon)
+(use-package evil-escape)
+(use-package expand-region)
+(use-package projectile)
+(use-package ivy)
+(use-package vertico)
+(use-package expand-region)
+(use-package company)
+(use-package evil-collection
+  :after evil
+  :config
+  (setq evil-want-integration t)
+  (evil-collection-init))
+
+(use-package vterm)
+(use-package vterm-toggle
+  :config
+	(setq vterm-toggle-fullscreen-p nil)
+	(add-to-list 'display-buffer-alist
+				'((lambda (buffer-or-name _)
+					(let ((buffer (get-buffer buffer-or-name)))
+						(with-current-buffer buffer
+						(or (equal major-mode 'vterm-mode)
+							(string-prefix-p vterm-buffer-name (buffer-name buffer))))))
+					(display-buffer-reuse-window display-buffer-at-bottom)
+					;;(display-buffer-reuse-window display-buffer-in-direction)
+					;;display-buffer-in-direction/direction/dedicated is added in emacs27
+					;;(direction . bottom)
+					;;(dedicated . t) ;dedicated is supported in emacs27
+					(reusable-frames . visible)
+					(window-height . 0.3)))
+  )
+
+(use-package lsp-mode
+  :hook (c-mode . lsp)
+  :hook (c++-mode . lsp)
+  :hook (csharp-mode . lsp)
+  :commands lsp)
+
+(use-package lsp-ui)
+(use-package flycheck)
+(use-package which-key
+  :config
+  (which-key-mode 1))
+(use-package evil-collection)
+(use-package lsp-ivy)
+(use-package avy)
+
+(use-package iedit
+:after evil
+:config
+  
+(define-key evil-motion-state-map (kbd "SPC s i") 'iedit-mode)
+(define-key evil-motion-state-map (kbd "SPC s s") 'lsp-ivy-workspace-symbol)
+;(evil-define-key 'motion 'global (kbd "SPC s i") 'iedit-mode)
+;(evil-define-key 'motion lsp-mode-map (kbd "SPC s s") 'lsp-ivy-workspace-symbol)
+)
 
 
 
@@ -33,38 +106,32 @@
 (setq visible-bell nil)
 
 
-(setq evil-want-keybinding nil)
 
 
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 
-(evil-mode 1)
-(evil-escape-mode 1)
-(git-gutter-mode 1)
+
 ;; (diff-hl-flydiff-mode 1)
 
 
-(require 'expand-region)
 
 (electric-pair-mode 1)
 
-(which-key-mode 1)
+(evil-escape-mode 1)
+
 
 (beacon-mode 1)
 (projectile-mode 1)
 (ivy-mode 1)
 
 (global-display-line-numbers-mode 1)
-
-(add-hook 'after-init-hook 'global-company-mode)
+(global-company-mode)
 (vertico-mode 1)
 (recentf-mode 1)
 
 
-;; loading themes
-(load "~/.emacs.d/theming.el")
 
 
 (setq-default evil-escape-key-sequence "aü")
@@ -73,45 +140,53 @@
 (global-set-key "\M-f" 'find-file)
 
 
-;;(global-set-key (kbd "SPC SPC") 'execute-extended-command)
-
-;; (define-key key-translation-map (kbd "SPC") 'event-apply-meta-modifier)
-
- 
-;;(define-key key-translation-map (kbd "SPC") 'event-apply-meta-modifier)
-(define-key evil-motion-state-map " " nil)
-(define-key evil-motion-state-map (kbd "SPC j j") 'avy-goto-char)
-(define-key evil-motion-state-map (kbd "SPC s i") 'iedit-mode)
 (define-key evil-motion-state-map (kbd "SPC SPC") 'execute-extended-command)
 
+; Navigation
+(define-key evil-motion-state-map (kbd "SPC j j") 'avy-goto-char)
+
+; Files
+(define-key evil-motion-state-map (kbd "SPC f f") 'find-file)
+(define-key evil-motion-state-map (kbd "SPC f d") 'dired)
+(define-key evil-motion-state-map (kbd "SPC f e d") 'open-config-file)
+
+; Windowing
 (define-key evil-motion-state-map (kbd "SPC w s v") 'split-window-vertically)
 (define-key evil-motion-state-map (kbd "SPC w s h") 'split-window-horizontally)
 (define-key evil-motion-state-map (kbd "SPC w a") 'delete-other-windows)
-
-(define-key evil-motion-state-map (kbd "SPC f f") 'find-file)
-(define-key evil-motion-state-map (kbd "SPC f d") 'dired)
-
+(define-key evil-motion-state-map (kbd "SPC w o") 'other-window)
 (define-key evil-motion-state-map (kbd "SPC w l") 'evil-window-right)
 (define-key evil-motion-state-map (kbd "SPC w h") 'evil-window-left)
 (define-key evil-motion-state-map (kbd "SPC w j") 'evil-window-down)
 (define-key evil-motion-state-map (kbd "SPC w k") 'evil-window-up)
 (define-key evil-motion-state-map (kbd "SPC w x") 'delete-window)
 
-; Selection bindings
+; Search
+(evil-define-key 'motion lsp-mode-map (kbd "SPC s s") 'lsp-ivy-workspace-symbol)
+
+; Edit
+(evil-define-key 'motion 'global (kbd "SPC s i") 'iedit-mode)
+
+; Selection
 (define-key evil-motion-state-map (kbd "SPC k k") 'er/expand-region)
 (define-key evil-motion-state-map (kbd "SPC k \"") 'er/mark-inside-quotes)
 (define-key evil-motion-state-map (kbd "SPC k p") 'er/mark-inside-pairs)
 
+; buffers
 (define-key evil-motion-state-map (kbd "SPC b x") 'kill-this-buffer)
 (define-key evil-motion-state-map (kbd "SPC b p") 'previous-buffer)
 (define-key evil-motion-state-map (kbd "SPC b n") 'next-buffer)
 
-(define-key evil-motion-state-map (kbd "SPC f e d") 'open-config-file)
+(define-key evil-motion-state-map (kbd "SPC b b") 'ivy-switch-buffer)
 
 
 ; Compilation
 
 (define-key evil-motion-state-map (kbd "SPC c r") 'compile)
+
+; Tools
+
+(define-key evil-motion-state-map (kbd "SPC t t") 'vterm-toggle)
 
 
 ;; PROJECTILE
@@ -120,30 +195,26 @@
 (define-key evil-motion-state-map (kbd "SPC p f") 'projectile-find-file)
 (define-key evil-motion-state-map (kbd "SPC p d") 'projectile-dired)
 (define-key evil-motion-state-map (kbd "SPC j j") 'avy-goto-char)
-(define-key evil-motion-state-map (kbd "SPC b b") 'ibuffer)
 
 
 ;; IEDIT
 
-(require 'iedit)
 (defun iedit-escape
     (interactive)
     (iedit-mode -1)
 )
+
+
 (define-key iedit-mode-keymap (kbd "<escape>") 'iedit-escape)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; NAVIGATION
 (with-eval-after-load 'company-mode
     (define-key company-active-map (kbd "C-j") 'company-select-next-or-abort)
     (define-key company-active-map (kbd "C-k") 'company-select-previous-or-abort)
 )
 
 (setq evil-emacs-state-modes (delq 'ibuffer-mode evil-emacs-state-modes))
-
-;;(global-set-key (kbd "C-j") 'ivy-next-line)
-;;(global-set-key (kbd "C-j") 'ivy-next-line)
 
 (define-key ivy-minibuffer-map (kbd "C-j") 'ivy-next-line)
 (define-key ivy-minibuffer-map (kbd "C-k") 'ivy-previous-line)
@@ -163,11 +234,12 @@
 
 (require 'lsp-mode)
 (add-hook 'csharp-mode-hook #'lsp)
-(add-hook 'after-init-hook #'global-flycheck-mode)
+;;(add-hook 'after-init-hook #'global-flycheck-mode)
 
 (with-eval-after-load 'lsp-mode
-    (define-key evil-motion-state-map (kbd "SPC s") lsp-command-map)
+    (define-key evil-motion-state-map (kbd "SPC l") lsp-command-map)
     (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
+
 (setq-default tab-width 4)
 
 (setq backup-directory-alist '(("." . "~/.emacs.d/backup"))
@@ -213,3 +285,4 @@
 
 
 (evil-collection-init 'term)
+
--- theming.el
+++ theming.el
@@ -1,9 +1,16 @@
-
 (global-hl-line-mode 1)
-(set-face-attribute 'hl-line nil :inherit nil :background "gray18")
+
+(set-face-attribute 'hl-line nil :inherit nil :background "gray20")
 
 (define-key special-event-map [config-changed-event] #'ignore)
 
-(load-theme 'deeper-blue t)
+(use-package reverse-theme
+  :config
+	(set-face-attribute 'hl-line nil :inherit nil :background "gray10")
+	(custom-set-faces
+		'(ivy-minibuffer-match-face-2 ((t (:background "gray20"))))))
+(load-theme 'reverse t)
 
-(set-frame-font "Consolas 14" nil t)
+(if (x-list-fonts "Consolas")
+    (set-frame-font "Consolas 14" nil t)
+    (message "Consolas font is not found. Using default font"))
